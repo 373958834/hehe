cb6cc84bdba43396a59961065df3dff4222ba3ef
doube <373958834@qq.com>
2019-07-11 09:15:30 +0800

    element-ui

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div id="app">
     App
+    <el-progress :text-inside="true" :stroke-width="26" :percentage="100"></el-progress>
+    <el-progress :text-inside="true" :stroke-width="24" :percentage="80" status="success"></el-progress>
+    <el-progress :text-inside="true" :stroke-width="22" :percentage="50" status="warning"></el-progress>
+    <el-progress :text-inside="true" :stroke-width="20" :percentage="30" status="exception"></el-progress>
   </div>
 </template>
 
 <script>
-
 export default {
   name: 'app'
 }
 </script>
 
 <style>
-
 </style>
