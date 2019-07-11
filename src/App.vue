b9109af719bc112596dd499571723990e5a762ac
doube <373958834@qq.com>
2019-07-11 11:30:48 +0800

    login页

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <div id="app">
-    App
-    <el-progress :text-inside="true" :stroke-width="26" :percentage="100"></el-progress>
-    <el-progress :text-inside="true" :stroke-width="24" :percentage="80" status="success"></el-progress>
-    <el-progress :text-inside="true" :stroke-width="22" :percentage="50" status="warning"></el-progress>
-    <el-progress :text-inside="true" :stroke-width="20" :percentage="30" status="exception"></el-progress>
+    <router-view></router-view>
   </div>
 </template>
 
---
+++ src/views/login/index.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="bgc">
+    <el-card class="login-box">
+      <img src="../../assets/images/logo_index.png" alt />
+      <!-- 账号:<el-input placeholder="请输入内容" v-model="input" clearable></el-input> -->
+      <el-form
+        :model="ruleForm"
+        status-icon
+        :rules="rules"
+        ref="ruleForm"
+        label-width="100px"
+        class="demo-ruleForm"
+      >
+        <el-form-item label="账号" prop="pass">
+          <el-input type="text" v-model="ruleForm.pass" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="checkPass">
+          <el-input type="password" v-model="ruleForm.checkPass" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submitForm('ruleForm')" class="ccc">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    var validatePass = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请输入账号"));
+      }
+    };
+    var validatePass2 = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请输入密码"));
+      }
+    };
+    return {
+      ruleForm: {
+        pass: "",
+        checkPass: ""
+      },
+      rules: {
+        pass: [{ validator: validatePass, trigger: "blur" }],
+        checkPass: [{ validator: validatePass2, trigger: "blur" }]
+      }
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          alert("submit!");
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    }
+  }
+};
+</script>
+
+<style>
+.bgc {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  background: url(../../assets/images/login_bg.jpg) no-repeat center / cover;
+}
+.login-box {
+  width: 400px;
+  height: 300px;
+  position: absolute;
+  background: url(../../assets/images/hh.jpeg) no-repeat center / cover;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
+img {
+  display: block;
+  width: 200px;
+  margin: 10px auto;
+}
+.ccc {
+  width: 100px;
+}
+.demo-ruleForm {
+  margin-top: 20px;
+  text-align: center;
+  padding-right: 40px;
+  opacity: .6;
+}
+.el-form-item__label {
+  color: #409eff;
+}
+</style>
